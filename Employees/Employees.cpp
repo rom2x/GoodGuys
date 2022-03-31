a62e289a665e626d3dbc12e8ff240e9197f988c0
ParkSeongGyu <bluelomu@gmail.com>
2022-03-31 19:48:40 +0900

    feat: Add 'unordered map' data struct at Employees
    사번을 key값으로 하는 unordered_map 객체를 추가하여 사번을 기반으로 Employee 포인터를 빠르게 찾을 수 있게 함

--- Employees/Employees.cpp
+++ Employees/Employees.cpp
@@ -1,7 +1,8 @@
-#include "Employees.h"
+﻿#include "Employees.h"
 
 #include <algorithm>
 #include <exception>
+#include <unordered_map>
 
 using namespace std;
 
@@ -30,6 +31,15 @@ public:
 
 private:
 	employeeList list;
+	unordered_map<unsigned, Employee*> map;
+
+	Employee* find(Employee& target)
+	{
+		auto search = map.find(target.employeeNumber);
+		if (search == map.end())
+			throw invalid_argument("존재하지 않는 직원");
+		return map.find(target.employeeNumber)->second;
+	}
 };
 
 Employees* createEmployees()
