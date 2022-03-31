8f4ba7ad305863ae7c27d8b87297ac6fd1a7b333
Juwon Byun <bjw0121@hanmail.net>
2022-03-31 13:15:58 +0900

    feat: Search 원형

---
+++ GoodGuys/Search.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <iostream>
+
+class SearchResult
+{
+
+};
+
+class SearchCondition
+{
+
+};
+
+class Search {
+public:
+	
+	SearchResult SearchProcess(SearchCondition searchCondition)
+	{
+
+	}
+
+
+};
+
