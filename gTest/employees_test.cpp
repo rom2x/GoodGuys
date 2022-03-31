c37d2db763bc9cc38c2ca0b150f55aa6fa372442
ParkSeongGyu <bluelomu@gmail.com>
2022-04-01 07:41:28 +0900

    feat: Add constructor of Employee
    파일로 부터 받아오는 Employee의 데이터 형식이 string 타입임으로 string 입력으로 부터 Employee 객체가 생성되도록 생성자와 변환 함수들을 추가함

--- gTest/employees_test.cpp
+++ gTest/employees_test.cpp
@@ -1,4 +1,4 @@
-#include "pch.h"
+ï»¿#include "pch.h"
 #include <algorithm>
 #include "../Employees/Employees.cpp"
 
@@ -17,22 +17,22 @@ protected:
     virtual void SetUp() override
     {
         Employee test_employees[] = {
-            { 15123099, "VXIHXOTH", "JHOP", CL::CL3, 3112, 2609, 19771211, CERTI::ADV},
-            { 17112609, "FB", "NTAWR", CL::CL4, 5645,6122, 19861203, CERTI::PRO },
-            { 18115040, "TTETHU", "HBO", CL::CL3, 4581, 2050, 20080718, CERTI::ADV },
-            { 88114052, "NQ", "LVARW", CL::CL4, 4528, 3059, 19911021, CERTI::PRO },
-            { 19129568, "SRERLALH", "HMEF", CL::CL2, 3091, 9521, 19640910, CERTI::PRO },
-            { 17111236, "VSID", "TVO", CL::CL1, 3669, 1077, 20120718, CERTI::PRO },
-            { 18117906, "TWU", "QSOLT", CL::CL4, 6672, 7186, 20030413, CERTI::PRO },
-            { 8123556, "WN", "XV", CL::CL1, 7986, 5047, 20100614, CERTI::PRO },
-            { 2117175, "SBILHUT", "LDEXRI", CL::CL4, 2814,1699, 19950704, CERTI::ADV },
+            { "15123099", "VXIHXOTH", "JHOP", "CL3", "3112", "2609", "19771211", "ADV" },
+            { "17112609", "FB", "NTAWR", "CL4", "5645", "6122", "19861203", "PRO" },
+            { "18115040", "TTETHU", "HBO", "CL3", "4581", "2050", "20080718", "ADV"},
+            { "88114052", "NQ", "LVARW", "CL4", "4528", "3059", "19911021", "PRO"},
+            { "19129568", "SRERLALH", "HMEF", "CL2", "3091", "9521", "19640910", "PRO"},
+            { "17111236", "VSID", "TVO", "CL1", "3669", "1077", "20120718", "PRO"},
+            { "18117906", "TWU", "QSOLT", "CL4", "6672", "7186", "20030413", "PRO"},
+            { "81235356", "WN", "XV", "CL1", "7986", "5047", "20100614", "PRO"},
+            { "02117175", "SBILHUT", "LDEXRI", "CL4", "2814", "1699", "19950704", "ADV"},
         };
 
         for (auto t : test_employees) {
             db->add(t);
         }
 
-        cout << "Client »ı¼º" << endl;
+        cout << "Client ìƒì„±" << endl;
     }
 
     virtual void TearDown() override
@@ -46,14 +46,14 @@ protected:
 
 TEST(EmployeeTest, EmployeeOperator)
 {
-    Employee a = { 15123099, "VXIHXOTH", "JHOP", CL::CL3, 3112, 2609, 19771211, CERTI::ADV };
-    Employee b = { 18115040, "TTETHU", "HBO", CL::CL3, 4581, 2050, 20080718, CERTI::ADV };
+    Employee a = { "15123099", "VXIHXOTH", "JHOP", "CL3", "3112", "2609", "19771211", "ADV" };
+    Employee b = { "18115040", "TTETHU", "HBO", "CL3", "4581", "2050", "20080718", "ADV" };
 
     EXPECT_TRUE(a < b);
     EXPECT_FALSE(a > b);
 
-    Employee c = { 15123099, "VXIHXOTH", "JHOP", CL::CL3, 3112, 2609, 19771211, CERTI::ADV };
-    Employee d = { 2117175, "SBILHUT", "LDEXRI", CL::CL4, 2814,1699, 19950704, CERTI::ADV }; // 2000³â ÀÌ ÈÄ »ç¹ø¿¡ ´ëÇÑ ÄÉÀÌ½º Å×½ºÆ®
+    Employee c = { "81235356", "WN", "XV", "CL1", "7986", "5047", "20100614", "PRO" };
+    Employee d = { "02117175", "SBILHUT", "LDEXRI", "CL4", "2814", "1699", "19950704", "ADV" }; // 2000ë…„ ì´ í›„ ì‚¬ë²ˆì— ëŒ€í•œ ì¼€ì´ìŠ¤ í…ŒìŠ¤íŠ¸
 
     EXPECT_TRUE(c < d);
     EXPECT_FALSE(c > d);
@@ -70,7 +70,7 @@ TEST_F(clientTest, EmployeesTestAdd)
 {
     const employeeList* t = db->getEmployees();
 
-    Employee empl = { 12234575, "EOJOEI", "EWFWEF", CL::CL2, 5332, 6544, 20152623, CERTI::PRO };
+    Employee empl = { "12234575", "EOJOEI", "EWFWEF", "CL2", "5332", "6544", "20152623", "PRO" };
     db->add(empl);
 
     EXPECT_EQ(t->size(), 10);
@@ -80,20 +80,20 @@ TEST_F(clientTest, EmployeesTestDelete)
 {
     const employeeList* t = db->getEmployees();
 
-    Employee empl1 = { 17111236, "VSID", "TVO", CL::CL1, 3669, 1077, 20120718, CERTI::PRO };
+    Employee empl1 = { "17111236", "VSID", "TVO", "CL1", "3669", "1077", "20120718", "PRO" };
     db->del(empl1);
     EXPECT_EQ(t->size(), 8);
 
-    Employee empl2 = { 19129568, "SRERLALH", "HMEF", CL::CL2, 3091, 9521, 19640910, CERTI::PRO };
+    Employee empl2 = { "19129568", "SRERLALH", "HMEF", "CL2", "3091", "9521", "19640910", "PRO" };
     db->del(empl2);
     EXPECT_EQ(t->size(), 7);
 
-    Employee empl3 = { 32523452, "SRERLALH", "HMEF", CL::CL2, 3091, 9521, 19640910, CERTI::PRO };
+    Employee empl3 = { "32523452", "SRERLALH", "HMEF", "CL2", "3091", "9521", "19640910", "PRO" };
     EXPECT_ANY_THROW(db->del(empl3));
 }
 
 TEST_F(clientTest, EmployeesTestModify) {
-    Employee target = { 15123099, "VXIHXOTH", "JHOP", CL::CL3, 3112, 2609, 19771211, CERTI::ADV };
+    Employee target = { "15123099", "VXIHXOTH", "JHOP", "CL3", "3112", "2609", "19771211", "ADV" };
 
     db->modify(target, [](Employee& target) {target.lastName = "ABC"; });
 
@@ -103,6 +103,6 @@ TEST_F(clientTest, EmployeesTestModify) {
     EXPECT_NE(modified.lastName, "JHOP");
     EXPECT_EQ(modified.lastName, "ABC");
 
-    Employee nodata = { 23452212, "VXIHXOTH", "ABC", CL::CL3, 3112, 2609, 19771211, CERTI::ADV };
+    Employee nodata = { "23452212", "VXIHXOTH", "ABC", "CL3", "3112", "2609", "19771211", "ADV" };
     EXPECT_ANY_THROW(db->modify(nodata, [](Employee& target) {target.birth = 19730205; }));
 }
