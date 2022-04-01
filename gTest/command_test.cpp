d9bf43956ef719be09cc2ee6c2fab5324cd4f6f5
minsikson <tofu89223@gmail.com>
2022-04-01 17:30:55 +0900

    test: command 생성자 확인용 tc 추가

--- gTest/command_test.cpp
+++ gTest/command_test.cpp
@@ -1,41 +1,28 @@
-﻿#include "pch.h"
+#include "pch.h"
 
-
-#include "../Employees/employees.cpp"
 #include "../command/command.cpp"
 
 class CommandTest : public ::testing::Test
 {
 public:
     CommandTest() {
-        db = createEmployees();
-
-        add = new Add(db);
-        del = new Del(db);
-        search = new Search(db);
-        modify = new Modify(db);
+        db = CreateEmployees();
     }
 
     ~CommandTest() {
         delete db;
 
-        delete add;
-        delete del;
-        delete search;
-        delete modify;
     }
 
 protected:
     virtual void SetUp() override
     {
-        testCmdLine.push_back("");
-        testCmdLine.push_back("");
-        testCmdLine.push_back("");
         testCmdLine.push_back("15123099");
-        testCmdLine.push_back("VXIHXOTH JHOP");
+        testCmdLine.push_back("VXIHXOTH");
+        testCmdLine.push_back("JHOP");
         testCmdLine.push_back("CL3");
-        testCmdLine.push_back("010 - 3112 - 2609");
-        testCmdLine.push_back("19771211");
+        testCmdLine.push_back("3112");
+        testCmdLine.push_back("2609");
         testCmdLine.push_back("ADV");
     }
 
@@ -49,13 +36,18 @@ protected:
     vector<Employee> testEmployee;
 
     Employees* db;
-
-    Command* add;
-    Command* del;
-    Command* search;
-    Command* modify;
 };
 
-TEST_F(CommandTest, command_test_1) {
-    EXPECT_NO_THROW(add->Process(testCmdLine));
+TEST_F(CommandTest, command_test_0_constructor)
+{
+    Command* add = new Add(testCmdLine);
+    Command* del = new Del(testCmdLine);
+    Command* search = new Search(testCmdLine);
+    Command* modify= new Modify(testCmdLine);
+}
+
+TEST_F(CommandTest, command_test_0_command_process)
+{
+    //Command* add = new Add(testCmdLine);
+    //EXPECT_NO_THROW(add->Process(db));
 }
