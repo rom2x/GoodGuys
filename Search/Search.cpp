bafbb10641c62ae892e4fb5039db657e549f4954
Juwon Byun <bjw0121@hanmail.net>
2022-04-01 16:24:26 +0900

    refactor: change SearchOut class to employeeList

--- Search/Search.cpp
+++ Search/Search.cpp
@@ -6,93 +6,89 @@ void Search::SetEmployeeList(employeeList* list) {
 	this->employee_db = list;
 }
 
-void Search::SearchByFirstName(SearchInput in, SearchOutput& out) {
-	employeeList::iterator employee;
-	for (employee = this->employee_db->begin(); employee != this->employee_db->end(); employee++)
+void Search::SearchByFirstName(SearchInput in, employeeList& out) {
+	for (auto employee : *(this->employee_db) )
 	{
-		if (employee->firstName == in.search_pattern)
-			out.push_back(&(*employee));
+		if (employee.firstName == in.search_pattern) {
+			out.emplace_back(employee);
+		}
 		if (in.is_search_and_p && out.size() == 5)
 			break;
 	}
 }
 
-void Search::SearchByLastName(SearchInput in, SearchOutput& out) {
-	employeeList::iterator employee;
-	for (employee = this->employee_db->begin(); employee != this->employee_db->end(); employee++)
-	{
-		if (employee->lastName == in.search_pattern)
-			out.push_back(&(*employee));
+void Search::SearchByLastName(SearchInput in, employeeList& out) {
+	for (auto employee : *(this->employee_db)) {
+		if (employee.lastName == in.search_pattern) {
+			out.emplace_back(employee);
+		}
+		if (in.is_search_and_p && out.size() == 5) 
+			break;
+	}
+}
+
+void Search::SearchByPhoneNumMid(SearchInput in, employeeList& out) {
+	for (auto employee : *(this->employee_db)) {
+		if (employee.phoneNumMid == stoi(in.search_pattern)) {
+			out.emplace_back(employee);
+		}
 		if (in.is_search_and_p && out.size() == 5)
 			break;
 	}
 }
 
-void Search::SearchByPhoneNumMid(SearchInput in, SearchOutput& out) {
-	employeeList::iterator employee;
-	for (employee = this->employee_db->begin(); employee != this->employee_db->end(); employee++)
-	{
-		if (employee->phoneNumMid == stoi(in.search_pattern))
-			out.push_back(&(*employee));
+void Search::SearchByPhoneNumLast(SearchInput in, employeeList& out) {
+	for (auto employee : *(this->employee_db)) {
+		if (employee.phoneNumLast == stoi(in.search_pattern)) {
+			out.emplace_back(employee);
+		}
 		if (in.is_search_and_p && out.size() == 5)
 			break;
 	}
 }
 
-void Search::SearchByPhoneNumLast(SearchInput in, SearchOutput& out) {
-	employeeList::iterator employee;
-	for (employee = this->employee_db->begin(); employee != this->employee_db->end(); employee++)
-	{
-		if (employee->phoneNumLast == stoi(in.search_pattern))
-			out.push_back(&(*employee));
+void Search::SearchByBirthYear(SearchInput in, employeeList& out) {
+	for (auto employee : *(this->employee_db)) {
+		unsigned year = employee.birth / 10000;
+		if (year == stoi(in.search_pattern)) {
+			out.emplace_back(employee);
+		}
 		if (in.is_search_and_p && out.size() == 5)
 			break;
 	}
 }
 
-void Search::SearchByBirthYear(SearchInput in, SearchOutput& out) {
-	employeeList::iterator employee;
-	for (employee = this->employee_db->begin(); employee != this->employee_db->end(); employee++)
-	{
-		unsigned year = employee->birth / 10000;
-		if (year == stoi(in.search_pattern))
-			out.push_back(&(*employee));
-		if (in.is_search_and_p && out.size() == 5)
-			break;
-	}
-}
-
-void Search::SearchByBirthMonth(SearchInput in, SearchOutput& out) {
-	employeeList::iterator employee;
-	for (employee = this->employee_db->begin(); employee != this->employee_db->end(); employee++)
-	{
-		unsigned month = employee->birth % 10000 / 100;
-		if (month == stoi(in.search_pattern))
-			out.push_back(&(*employee));
-		if(month > 12)
+void Search::SearchByBirthMonth(SearchInput in, employeeList& out) {
+	for (auto employee : *(this->employee_db)) {
+		unsigned month = employee.birth % 10000 / 100;
+		if (month == stoi(in.search_pattern)) {
+			out.emplace_back(employee);
+		}
+		if (month > 12) {
 			throw std::out_of_range("month Range Error!");
+		}
 		if (in.is_search_and_p && out.size() == 5)
 			break;
 	}
 }
 
-void Search::SearchByBirthDay(SearchInput in, SearchOutput& out) {
-	employeeList::iterator employee;
-	for (employee = this->employee_db->begin(); employee != this->employee_db->end(); employee++)
-	{
-		unsigned day = employee->birth % 100;
-		if (day == stoi(in.search_pattern))
-			out.push_back(&(*employee));
-		if (day > 31)
+void Search::SearchByBirthDay(SearchInput in, employeeList& out) {
+	for (auto employee : *(this->employee_db)) {
+		unsigned day = employee.birth % 100;
+		if (day == stoi(in.search_pattern)) {
+			out.emplace_back(employee);
+		}
+		if (day > 31) {
 			throw std::out_of_range("Day Range Error!");
+		}
 		if (in.is_search_and_p && out.size() == 5)
 			break;
 	}
 }
 
 
-SearchOutput Search::DoSearch(SearchInput in) {
-	SearchOutput searchOutput;
+employeeList Search::DoSearch(SearchInput in) {
+	employeeList searchOutput;
 
 	switch (in.search_type)	{
 	case SearchType::FIRST_NAME:		Search::SearchByFirstName(in, searchOutput);	break;
