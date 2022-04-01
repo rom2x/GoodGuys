7506ceea843bdafa4ad7e9619a95864073024275
seungrae.kim <tmdfo1228@gmail.com>
2022-04-01 22:03:20 +0900

    feat: Parser TDD 코드 작성

---
+++ GoodGuys/input_parser_manager.cpp
@@ -0,0 +1,13 @@
+#include "input_parser_manager.h"
+
+vector<Command*> InputParserManager::GetCommandList(vector<string> inputstrings) {
+	vector<Command*> result;
+
+	return result;
+}
+
+vector <string> InputParserManager::GetEachLineParsedStrings(string astring) {
+	vector<string> result;
+
+	return result;
+}
--- gTest/fileinput_test.cpp
+++ gTest/fileinput_test.cpp
@@ -1,5 +1,6 @@
 #include "pch.h"
 #include "../GoodGuys/file_input_manager.cpp"
+#include "../GoodGuys/input_parser_manager.cpp"
 
 class FileInputManagerTest : public ::testing::Test
 {
@@ -22,3 +23,78 @@ TEST_F(FileInputManagerTest, InvalidFile) {
     EXPECT_FALSE(inputmanager_->IsFileValid());
 }
 
+class InputParserManagerTest : public ::testing::Test
+{
+protected:
+    virtual void SetUp() override
+    {
+        parsemanager_ = new InputParserManager();
+        inputstrings_.push_back("ADD, , , ,15123099,VXIHXOTH JHOP,CL3,010-3112-2609,19771211,ADV");
+        inputstrings_.push_back("SCH,-p,-d, ,birthday,04");
+        inputstrings_.push_back("MOD,-p, , ,name,FB NTAWR,birthday,20050520");
+        inputstrings_.push_back("DEL,-p,-l, ,name,MPOSXU");
+
+        addoutputstring_.push_back("ADD");
+        addoutputstring_.push_back(" ");
+        addoutputstring_.push_back(" ");
+        addoutputstring_.push_back(" ");
+        addoutputstring_.push_back("15123099");
+        addoutputstring_.push_back("VXIHXOTH JHOP");
+        addoutputstring_.push_back("CL3");
+        addoutputstring_.push_back("010-3112-2609");
+        addoutputstring_.push_back("19771211");
+        addoutputstring_.push_back("ADV");
+
+        schoutputstring_.push_back("SCH");
+        schoutputstring_.push_back("-p");
+        schoutputstring_.push_back("-d");
+        schoutputstring_.push_back(" ");
+        schoutputstring_.push_back("birthday");
+        schoutputstring_.push_back("04");
+
+        modoutputstring_.push_back("MOD");
+        modoutputstring_.push_back("-p");
+        modoutputstring_.push_back(" ");
+        modoutputstring_.push_back(" ");
+        modoutputstring_.push_back("name");
+        modoutputstring_.push_back("FB NTAWR");
+        modoutputstring_.push_back("birthday");
+        modoutputstring_.push_back("20050520");
+
+        deloutputstring_.push_back("DEL");
+        deloutputstring_.push_back("-p");
+        deloutputstring_.push_back("-l");
+        deloutputstring_.push_back(" ");
+        deloutputstring_.push_back("name");
+        deloutputstring_.push_back("MPOSXU");
+    }
+
+    virtual void TearDown() override
+    {
+
+    }
+
+    InputParserManager* parsemanager_;
+    vector <string> inputstrings_;
+    vector <string> addoutputstring_;
+    vector <string> schoutputstring_;
+    vector <string> modoutputstring_;
+    vector <string> deloutputstring_;
+};
+
+
+TEST_F(InputParserManagerTest, AddStringTest) {
+    EXPECT_EQ(addoutputstring_, parsemanager_->GetEachLineParsedStrings(inputstrings_[0]));
+}
+
+TEST_F(InputParserManagerTest, SchStringTest) {
+    EXPECT_EQ(schoutputstring_, parsemanager_->GetEachLineParsedStrings(inputstrings_[1]));
+}
+
+TEST_F(InputParserManagerTest, ModStringTest) {
+    EXPECT_EQ(modoutputstring_, parsemanager_->GetEachLineParsedStrings(inputstrings_[2]));
+}
+
+TEST_F(InputParserManagerTest, DelStringTest) {
+    EXPECT_EQ(deloutputstring_, parsemanager_->GetEachLineParsedStrings(inputstrings_[3]));
+}
