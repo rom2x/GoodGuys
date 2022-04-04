580d0eeafe1fcb5d988dbdb87ede7597b5855c40
minsikson <tofu89223@gmail.com>
2022-04-04 16:35:58 +0900

    test: command::Process() 함수 동작 확인용 TC 추가

--- gTest/command_test.cpp
+++ gTest/command_test.cpp
@@ -1,6 +1,5 @@
 #include "pch.h"
-
-#include "../command/command.cpp"
+#include "../command/command.h"
 
 class CommandTest : public ::testing::Test
 {
@@ -11,19 +10,52 @@ public:
 
     ~CommandTest() {
         delete db;
-
     }
 
 protected:
     virtual void SetUp() override
     {
-        testCmdLine.push_back("15123099");
-        testCmdLine.push_back("VXIHXOTH");
-        testCmdLine.push_back("JHOP");
-        testCmdLine.push_back("CL3");
-        testCmdLine.push_back("3112");
-        testCmdLine.push_back("2609");
-        testCmdLine.push_back("ADV");
+        Employee test_employees[] = {
+            { "18050301", "KYUMOK KIM", "CL2","010-9777-6055","19980906","PRO"},
+        };
+
+        for (auto t : test_employees) {
+            db->Add(t);
+        }
+
+        add_cmd.push_back("ADD");
+        add_cmd.push_back("");
+        add_cmd.push_back("");
+        add_cmd.push_back("");
+        add_cmd.push_back("18050301");
+        add_cmd.push_back("KYUMOK KIM");
+        add_cmd.push_back("CL2");
+        add_cmd.push_back("010-9777-6055");
+        add_cmd.push_back("19980906");
+        add_cmd.push_back("PRO");
+
+        del_cmd.push_back("DEL");
+        del_cmd.push_back("");
+        del_cmd.push_back("");
+        del_cmd.push_back("");
+        del_cmd.push_back("name");
+        del_cmd.push_back("KYUMOK KIM");
+
+        search_cmd.push_back("SCH");
+        search_cmd.push_back("");
+        search_cmd.push_back("");
+        search_cmd.push_back("");
+        search_cmd.push_back("name");
+        search_cmd.push_back("KYUMOK KIM");
+
+        modify_cmd.push_back("MOD");
+        modify_cmd.push_back("");
+        modify_cmd.push_back("");
+        modify_cmd.push_back("");
+        modify_cmd.push_back("name");
+        modify_cmd.push_back("KYUMOK KIM");
+        modify_cmd.push_back("name");
+        modify_cmd.push_back("KYUMOK LEE");
     }
 
     virtual void TearDown() override
@@ -32,22 +64,46 @@ protected:
     }
 
 protected:
-    vector<string> testCmdLine;
-    vector<Employee> testEmployee;
+    vector<string> add_cmd;
+    vector<string> del_cmd;
+    vector<string> search_cmd;
+    vector<string> modify_cmd;
 
     Employees* db;
 };
 
 TEST_F(CommandTest, command_test_0_constructor)
 {
-    Command* add = new Add(testCmdLine);
-    Command* del = new Del(testCmdLine);
-    Command* search = new Search(testCmdLine);
-    Command* modify= new Modify(testCmdLine);
+    EXPECT_NO_THROW(new AddCommand(add_cmd));
+    EXPECT_ANY_THROW(new DelCommand(add_cmd));
+    EXPECT_ANY_THROW(new SearchCommand(add_cmd));
+    EXPECT_ANY_THROW(new ModifyCommand(add_cmd));
+
+    EXPECT_NO_THROW(new DelCommand(del_cmd));
+    EXPECT_NO_THROW(new SearchCommand(search_cmd));
+    EXPECT_NO_THROW(new ModifyCommand(modify_cmd));
 }
 
-TEST_F(CommandTest, command_test_0_command_process)
+TEST_F(CommandTest, command_test_1_command_0_add_process)
 {
-    //Command* add = new Add(testCmdLine);
-    //EXPECT_NO_THROW(add->Process(db));
+    Command* add = new AddCommand(add_cmd);
+    EXPECT_NO_THROW(add->Process(db));
+}
+
+TEST_F(CommandTest, command_test_1_command_1_del_process)
+{
+    Command* del = new DelCommand(del_cmd);
+    EXPECT_NO_THROW(del->Process(db));
+}
+
+TEST_F(CommandTest, command_test_1_command_2_search_process)
+{
+    Command* search = new SearchCommand(search_cmd);
+    EXPECT_NO_THROW(search->Process(db));
+}
+
+TEST_F(CommandTest, command_test_1_command_3_modify_process)
+{
+    Command* modify = new ModifyCommand(modify_cmd);
+    EXPECT_NO_THROW(modify->Process(db));
 }
