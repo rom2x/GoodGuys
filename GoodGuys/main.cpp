ace53fa3a6ad6692268551576a7cf4ab911c6c96
seungrae.kim <tmdfo1228@gmail.com>
2022-04-04 09:48:29 +0900

    feat: Output Result Stream 구현

--- GoodGuys/main.cpp
+++ GoodGuys/main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 
 #include "file_input_manager.h"
+#include "file_output_manager.h"
 #include "input_parser_manager.h"
 #include "../Command/Command.h"
 #include "../Employees/Employees.h"
@@ -14,6 +15,7 @@ int main(int argc, char* argv[])
 	const char* outputfilename = argv[2];
 
 	FileInputManager* inputmanager = new FileInputManager(inputfilename);
+	FileOutputManager* outputmanager = new FileOutputManager(outputfilename);
 	InputParserManager* parsermanager = new InputParserManager();
 
 	if (!inputmanager->IsFileValid()) return 0;
@@ -24,10 +26,12 @@ int main(int argc, char* argv[])
 	//Employees* employees = CreateEmployees();
 	for (auto a_command : command_list) {
 		//a_command->Process(employees);
+		outputmanager->WriteResultToFile("ResultString\n");
 	}
 
 	//delete employees;
 	delete inputmanager;
+	delete outputmanager;
 	delete parsermanager;
 
 
